af850e22a84f5c4f1c70f371c2aaac82d432dfa2
kd3-80148 <joshiuunnati@gmail.com>
2024-02-07 22:28:47 +0530

    trail commit

--- Server/src/test/java/com/app/dao/UserEntityDaoTest.java
+++ Server/src/test/java/com/app/dao/UserEntityDaoTest.java
@@ -30,15 +30,15 @@ class UserEntityDaoTest {
 
 	@Test
 	void testAddUsers() {
-		List<UserEntity> list = List.of(
-				new UserEntity("a1", "b1", "a1@gmail.com", enc.encode("12345"),"9465823452", UserRole.ROLE_ADMIN,"146372983563"),
-				new UserEntity("a2", "b2", "a2@gmail.com", enc.encode("2345"),"9465823451", UserRole.ROLE_USER,"146372983562"),
-				new UserEntity("a3", "b3", "a3@gmail.com", enc.encode("1345"), "9465823453",UserRole.ROLE_USER,"146372983561"));
+	//	List<UserEntity> list = List.of(
+//				new UserEntity("a1", "b1", "a1@gmail.com", enc.encode("12345"),"9465823452", UserRole.ROLE_ADMIN,"146372983563"),
+//				new UserEntity("a2", "b2", "a2@gmail.com", enc.encode("2345"),"9465823451", UserRole.ROLE_USER,"146372983562"),
+//				new UserEntity("a3", "b3", "a3@gmail.com", enc.encode("1345"), "9465823453",UserRole.ROLE_USER,"146372983561"));
 //				new UserEntity("a1", "b1", "a1@gmail.com", "12345","9465823452", UserRole.ROLE_ADMIN,"146372983563"),
 //				new UserEntity("a2", "b2", "a2@gmail.com", "2345","9465823451", UserRole.ROLE_USER,"146372983562"),
 //				new UserEntity("a3", "b3", "a3@gmail.com", "1345", "9465823453",UserRole.ROLE_USER,"146372983561"));
-		List<UserEntity> list2 = userRepo.saveAll(list);
-		assertEquals(3, list2.size());
+//		List<UserEntity> list2 = userRepo.saveAll(list);
+//		assertEquals(3, list2.size());
 
 	}
 
