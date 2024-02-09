902ab90d783ab38e785d61c081cfa29b6a8186e1
kd3-80148 <joshiuunnati@gmail.com>
2024-02-09 18:20:33 +0530

    cancelling Functionality and Exception Handling

--- Server/src/main/java/com/app/service/DarshanServiceImpl.java
+++ Server/src/main/java/com/app/service/DarshanServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.app.custom_Exceptions.ResourceNotFoundException;
 import com.app.dao.DarshanDao;
 import com.app.dto.ApiResponse;
 import com.app.dto.DarshanDTO;
@@ -32,7 +33,6 @@ public class DarshanServiceImpl implements DarshanService {
 		Darshan persistentDarshan = darshanDao.save(darshanEntity);
 		return mapper.map(persistentDarshan, DarshanDTO.class);
 
-		
 	}
 
 	@Override
@@ -40,18 +40,16 @@ public class DarshanServiceImpl implements DarshanService {
 	
 		List<Darshan> darshanList = darshanDao.findByUserId(darshanId);
 		return darshanList.stream().map(darshan -> mapper.map(darshan, DarshanDTO.class)).collect(Collectors.toList());
-
 	}
 
 	@Override
 	public ApiResponse deleteDarshanBookingById(Long id) {
-		//Darshan darshan = darshanDao.findById(id).orElseThrow(() -> new ResourceNotFoundException("Invalidid"));
-
-		// Before deleting emp rec , delete it's child rec from ProjectEmpDetails
-//		projectEmpRepo.deleteByMyEmployeeId(empId);// yet to be tested
-//		empRepo.delete(emp);// yet to be tested
-//		return new ApiResponse("Emp Details of emp with ID " + emp.getId() + " deleted....");
-		return null;
+		Darshan darshan = darshanDao.findById(id).
+				orElseThrow(() -> new ResourceNotFoundException("Invalid emp id"));
+		
+		darshanDao.delete(darshan);
+		return new ApiResponse("Darshan Details of dasrhan with ID " + darshan.getId() + " deleted....");
+		
 	}
 
 	
