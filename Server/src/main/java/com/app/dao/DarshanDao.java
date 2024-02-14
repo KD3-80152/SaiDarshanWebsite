aa5daa3af1c50367eed9b92918a973fcf86eada2
PRASHANT KUMAR <kd3-80083>
2024-02-14 18:06:29 +0530

    changes

--- Server/src/main/java/com/app/dao/DarshanDao.java
+++ Server/src/main/java/com/app/dao/DarshanDao.java
@@ -3,7 +3,10 @@ package com.app.dao;
 import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.app.entities.Darshan;
+import com.app.entities.TimeSlot;
 
 public interface DarshanDao extends JpaRepository<Darshan, Long> {
 	
