97d949e6290fe214b7d146a0903ae00fc1158e1a
kd3-80148 <joshiuunnati@gmail.com>
2024-02-16 21:45:29 +0530

    Darshan functionality updated

--- Server/src/main/java/com/app/dao/DarshanDao.java
+++ Server/src/main/java/com/app/dao/DarshanDao.java
@@ -7,23 +7,27 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.app.entities.Darshan;
+import com.app.entities.TimeEnum;
 import com.app.entities.TimeSlot;
 
 public interface DarshanDao extends JpaRepository<Darshan, Long> {
 	
 	List<Darshan> findByUserId(Long userId);
 	
+	List<Darshan> findByBookingDateAndTimeSlot(LocalDate date,TimeEnum timeSlot);
 
 	//@Query("select d from Darshan d where d.bookingDate=:bookingDate and d.timeSlot = :timeSlot")
 	List<Darshan> findByBookingDateAndTimeSlot(Long bookingDateId , Long TimeSlotId);
-
-
+	
+	@Query("select d.timeSlot from Darshan d where d.bookingDate = :date group by a.timeSlot having sum(d.persons)>=5")
+	List<TimeEnum> findAllTimeSlotsByBookingDate(LocalDate date);
 
 //	@Query("select d from Darshan d order by d.date
 //	List<Darshan> findAllOrderedByDateAsc();
 
-
+	@Query("select d.bookingDate from Darshan d group by d.bookingDate having sum(d.persons)>=30")
+	List<LocalDate> findAllBookingDatesByPersons();
 	
-	@Query("select new com.app.entities.Darshan( d.timeSlot) from Darshan d where d.bookingDate = :date and d.counter < 5" )
-	List<TimeSlot> FindTimeSlotsByBookingDateAndCounter(LocalDate date);
+//	@Query("select new com.app.entities.Darshan( d.timeSlot) from Darshan d where d.bookingDate = :date and d.counter < 5" )
+//	List<TimeSlot> FindTimeSlotsByBookingDateAndCounter(LocalDate date);
 }
